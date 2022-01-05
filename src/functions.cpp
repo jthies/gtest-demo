555fbf3a587837287c37a9d1aad4b9cab78ba760
Jonas Thies <16190001+jthies@users.noreply.github.com>
2022-01-05 17:08:36 +0100

    add comments to add and subtract functions

--- src/functions.cpp
+++ src/functions.cpp
@@ -2,6 +2,9 @@
 
 double add_numbers(const double f1, const double f2) { return f1 + f2; }
 
+// This function subtracts f1 from f2 and returns the result
 double subtract_numbers(const double f1, const double f2) { return f1 - f2; }
 
+// This function adds f1 and f2 and returns the result
 double multiply_numbers(const double f1, const double f2) { return f1 * f2; }
+
